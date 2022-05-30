4e1539d6ccf535eec27601c994b97d5562f8d09f
Nailasya <3103120157@student.smktelkom-pwt.sch.id>
2022-05-30 16:02:26 +0700

    modul 1&2

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Naila Syakirotul Rizkiyah",
+        "email" => "syakirotulnaila@gmail.com",
+        "gambar" => "naila.jpg"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
+});
